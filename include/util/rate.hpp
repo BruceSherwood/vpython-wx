ad9e651bc3dae180631feb990e5840591ec95e76
BruceSherwood <bruce.sherwood@gmail.com>
2012-11-23 12:19:45 -0700

    Make it possible to run from terminal; wait() now takes 0 or 1 argument (in seconds)

--- include/util/rate.hpp
+++ include/util/rate.hpp
@@ -6,15 +6,9 @@
 
 namespace cvisual {
 
-// This function is stateful and allows an application to control its loop
-// execution rate.  When calling rate() once per iteration, rate inserts a small
-// delay that is calibrated such that the loop will execute at about 'freq'
-// iterations per second.
-//void rate( const double& freq);
-
 using namespace boost::python;
 
-typedef void (wait_t)(void);
+typedef void (wait_t)(double);
 
 void set_wait(object obj);
 
