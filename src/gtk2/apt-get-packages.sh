547b8b31829684bc2509a2296736034ef4854669
Steve Spicklemire <steve@spvi.com>
2013-01-12 23:13:05 -0700

    clean up some documentation

--- src/gtk2/apt-get-packages.sh
+++ src/gtk2/apt-get-packages.sh
@@ -1,11 +1,17 @@
-sudo apt-get -q -y update
-sudo apt-get -q -y install git
-sudo apt-get -q -y install libgtk2.0-dev
-sudo apt-get -q -y install libgtkglextmm-x11-1.2-dev
-sudo apt-get -q -y install libgtkmm-2.4-dev
-sudo apt-get -q -y install python-dev
-sudo apt-get -q -y install python-setuptools
-sudo apt-get -q -y install python-numpy
-sudo apt-get -q -y install libboost-python-dev
-sudo apt-get -q -y install libboost-signals-dev
-sudo apt-get -q -y install libghc-gstreamer-dev
+#!/bin/bash
+#
+# Helper script to grab all the packages vpython needs Run as root.
+#
+apt-get -q -y update
+apt-get -q -y install git
+apt-get -q -y install libgtk2.0-dev
+apt-get -q -y install libgtkglextmm-x11-1.2-dev
+apt-get -q -y install libgtkmm-2.4-dev
+apt-get -q -y install python-dev
+apt-get -q -y install python-setuptools
+apt-get -q -y install python-numpy
+apt-get -q -y install libboost-python-dev
+apt-get -q -y install libboost-signals-dev
+apt-get -q -y install libghc-gstreamer-dev
+apt-get -q -y install idle
+
